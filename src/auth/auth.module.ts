5eedf4ec8e55232850a46389796397a445335d5a
mateo <matmanfredi.99@gmail.com>
2024-04-22 23:36:37 -0300

    new env var to controll token expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { JWT_SECRET } from '../config';
+import { JWT_SECRET, JWT_TIME } from '../config';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [UsersModule , JwtModule.register({
     global:true,
     secret:JWT_SECRET.secret,
-    signOptions:{expiresIn:'180s'}
+    signOptions:{expiresIn: JWT_TIME || '1h'}
   })],
   providers: [AuthService],
   controllers: [AuthController]
--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,7 @@ dotenv.config();
 
 export const MONGO_STRING_CONNECTION = process.env.MONGO_STRING_CONNECTION;
 export const PORT = process.env.PORT;
-export const JWT_SECRET = {secret : process.env.JWT_SECRET }
+export const JWT_SECRET = {secret : process.env.JWT_SECRET };
+export const JWT_TIME = process.env.JWT_TIME;
 export const STARWARS_API = process.env.STARWARS_API
+
